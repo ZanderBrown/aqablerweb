bc6b46980e5ae793735161f8aebb227233a18a66
Zander Brown <zbrown@gnome.org>
2019-07-20 23:28:03 +0100

    Use prebuild favicons

--- gulpfile.ts
+++ gulpfile.ts
@@ -1,9 +1,5 @@
 const webpack = require('webpack-stream');
 const lessc = require('gulp-less');
-let fav = require('gulp-real-favicon');
-const generateFavicon = fav.generateFavicon;
-const injectFaviconMarkups = fav.injectFaviconMarkups;
-const readFileSync = require('fs').readFileSync;
 const mincss = require('gulp-clean-css');
 const minhtml = require('gulp-htmlmin');
 const minsvg = require('gulp-svgmin');
@@ -20,78 +16,8 @@ export function less(cb: Function) {
 	cb();
 };
 
-var FAVICON_DATA_FILE = 'faviconData.json';
-export function favicons(done: Function) {
-	generateFavicon({
-		masterPicture: './favicon.png',
-		dest: './icons',
-		iconsPath: './',
-		design: {
-			ios: {
-				pictureAspect: 'backgroundAndMargin',
-				backgroundColor: '#3030d8',
-				margin: '0%',
-				assets: {
-					ios6AndPriorIcons: false,
-					ios7AndLaterIcons: false,
-					precomposedIcons: true,
-					declareOnlyDefaultIcon: true
-				},
-				appName: 'Aqabler'
-			},
-			desktopBrowser: {},
-			windows: {
-				pictureAspect: 'whiteSilhouette',
-				backgroundColor: '#3030d8',
-				onConflict: 'override',
-				assets: {
-					windows80Ie10Tile: false,
-					windows10Ie11EdgeTiles: {
-						small: true,
-						medium: true,
-						big: true,
-						rectangle: true
-					}
-				},
-				appName: 'Aqabler'
-			},
-			androidChrome: {
-				pictureAspect: 'noChange',
-				themeColor: '#3030d8',
-				manifest: {
-					name: 'Aqabler',
-					startUrl: 'https://zanderbrown.github.io/aqablerweb/',
-					display: 'standalone',
-					orientation: 'notSet',
-					onConflict: 'override',
-					declared: true
-				},
-				assets: {
-					legacyIcon: false,
-					lowResolutionIcons: false
-				}
-			},
-			safariPinnedTab: {
-				pictureAspect: 'blackAndWhite',
-				threshold: 66.40625,
-				themeColor: '#3030d8'
-			}
-		},
-		settings: {
-			compression: 2,
-			scalingAlgorithm: 'Mitchell',
-			errorOnImageTooSmall: false,
-			readmeFile: false,
-			htmlCodeFile: false,
-			usePathAsIs: false
-		},
-		markupFile: FAVICON_DATA_FILE
-	}, done);
-};
-
 export function html(cb: Function) {
 	src(['index.html'])
-		.pipe(injectFaviconMarkups(JSON.parse(readFileSync(FAVICON_DATA_FILE)).favicon.html_code))
 		.pipe(minhtml({ collapseWhitespace: true }))
 		.pipe(dest(output));
 	cb();
